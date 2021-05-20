bfebb82b866498a2ffacb921069dbb708eed76a4
David <Davidchincharashvili@gmail.com>
2021-05-20 23:10:12 +0400

    basic setup with db, view, layout db - current sqlite3

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,12 @@ class Home extends BaseController
 {
 	public function index()
 	{
-		return view('welcome_message');
+		// var_dump($_ENV);
+		$parser = \Config\Services::parser();
+		$data = [
+			'page_title' => 'Your title'
+		];
+	
+		return view('home', $data);
 	}
 }
