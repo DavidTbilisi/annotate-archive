7005b9ee17f890b4658fd2b20d462bb188eeef0f
David Chincharashvili <davidchincharashvili@gmail.com>
2021-07-23 17:06:22 +0400

    small changes book_separation, document

--- app/Views/book_separation/index.php
+++ app/Views/book_separation/index.php
@@ -33,7 +33,8 @@ function checkbox($data = '', string $value = '', bool $checked = false, $extra 
 
 
     <style>
-        #book-separation form span {
+
+        #book-separation form #print {
             position: absolute;
             right: -15px;
             top: -35px;
@@ -46,7 +47,7 @@ function checkbox($data = '', string $value = '', bool $checked = false, $extra 
     <div class="uk-container uk-container-xlarge uk-section" id="book-separation">
 
         <div uk-grid>
-            <div class="uk-width-4-5@m uk-padding">
+            <div class="uk-width-3-5@m uk-padding">
                 <div class="uk-card">
                     <form class="uk-form-stacked">
 
@@ -57,15 +58,15 @@ function checkbox($data = '', string $value = '', bool $checked = false, $extra 
                             </div>
                         </div>
 
-                        <span uk-icon="icon: print" class="uk-icon-button uk-margin-small-right"></span>
+                        <span id="print" uk-icon="icon: print" class="uk-icon-button uk-margin-small-right"></span>
 
                         <div class="uk-flex uk-flex-between uk-child-width-expand">
 
                             <div>
                                 <div class="uk-margin">
-                                    <label class="uk-form-label" for="form-stacked-text">დონე</label>
+                                    <label class="uk-form-label" for="form-stacked-text1">დონე</label>
                                     <div class="uk-form-controls">
-                                        <input class="uk-input" id="form-stacked-text" type="text"
+                                        <input class="uk-input" id="form-stacked-text1" type="text"
                                                placeholder="Some text...">
                                     </div>
                                 </div>
@@ -73,9 +74,9 @@ function checkbox($data = '', string $value = '', bool $checked = false, $extra 
 
                             <div>
                                 <div class="uk-margin">
-                                    <label class="uk-form-label" for="form-stacked-text">პრეფიქსი</label>
+                                    <label class="uk-form-label" for="form-stacked-text2">პრეფიქსი</label>
                                     <div class="uk-form-controls">
-                                        <input class="uk-input" id="form-stacked-text" type="text"
+                                        <input class="uk-input" id="form-stacked-text2" type="text"
                                                placeholder="Some text...">
                                     </div>
                                 </div>
@@ -83,18 +84,18 @@ function checkbox($data = '', string $value = '', bool $checked = false, $extra 
 
                             <div>
                                 <div class="uk-margin">
-                                    <label class="uk-form-label" for="form-stacked-text">ნომერი</label>
+                                    <label class="uk-form-label" for="form-stacked-text3">ნომერი</label>
                                     <div class="uk-form-controls">
-                                        <input class="uk-input" id="form-stacked-text" type="text"
+                                        <input class="uk-input" id="form-stacked-text3" type="text"
                                                placeholder="Some text...">
                                     </div>
                                 </div>
                             </div>
                             <div>
                                 <div class="uk-margin">
-                                    <label class="uk-form-label" for="form-stacked-text">სუფიქსი</label>
+                                    <label class="uk-form-label" for="form-stacked-text4">სუფიქსი</label>
                                     <div class="uk-form-controls">
-                                        <input class="uk-input" id="form-stacked-text" type="text"
+                                        <input class="uk-input" id="form-stacked-text4" type="text"
                                                placeholder="Some text...">
                                     </div>
                                 </div>
@@ -103,12 +104,14 @@ function checkbox($data = '', string $value = '', bool $checked = false, $extra 
                         </div>
 
                         <div class="uk-text-right">
-                            <button class="uk-button uk-button-primary uk-display-inline">Save</button>
+                            <button class="uk-button uk-button-primary uk-display-inline">შენახვა</button>
                         </div>
 
                         <hr class="uk-divider-icon">
 
                     </form>
+
+
                     <table class="uk-table display" id="example" style="width:100%">
                         <thead>
                         <tr>
@@ -116,6 +119,7 @@ function checkbox($data = '', string $value = '', bool $checked = false, $extra 
                             <th>პრეფიქსი</th>
                             <th>ნომერი</th>
                             <th>სუფიქსი</th>
+                            <th>ქმედება</th>
                         </tr>
                         </thead>
                         <tbody>
@@ -125,6 +129,7 @@ function checkbox($data = '', string $value = '', bool $checked = false, $extra 
                                 <td><?= $item ?> პრეფიქსი</td>
                                 <td><?= random_int(1, 999) ?></td>
                                 <td><?= $item ?> სუფიქსი</td>
+                                <td class="uk-text-center"><a class="uk-text-danger" title="წაშლა" href="" uk-icon="icon: trash"></a></td>
                             </tr>
                         <?php endforeach; ?>
                         </tbody>
@@ -133,25 +138,74 @@ function checkbox($data = '', string $value = '', bool $checked = false, $extra 
 
                 </div>
             </div>
-            <div class="uk-width-1-5@m ">
+            <div class="uk-width-2-5@m ">
                 <div class="buttons uk-flex uk-flex-around uk-padding">
-                    <div class="uk-button uk-button-danger">Cancel</div>
-                    <div class="uk-button uk-button-primary">Save</div>
+                    <div class="uk-button uk-button-danger">გაუქმება</div>
+                    <div class="uk-button uk-button-primary">შენახვა</div>
                 </div>
 
-                <div class="dates uk-flex uk-flex-between">
-                    <div>
-                        <label for="startdate">Start Date</label>
-                        <input type="date" name="startdate" id="startdate">
+                <div class="dates">
+                    <div class="uk-flex uk-flex-between">
+                        <div>
+                            <label for="start_day">საწყისი დღე</label>
+                            <input name="start_day" class="uk-input" id="start_day" type="number" min="1" max="31">
+                        </div>
+                        <div>
+                            <label for="start_month">საწყისი თვე</label>
+                            <select name="start_month" class="uk-select" id="start_month">
+                                <option value="">აირჩიე თვე</option>
+                                <option value="1">იანვარი</option>
+                                <option value="2">თებერვალი</option>
+                                <option value="3">მარტი</option>
+                                <option value="4">აპრილი</option>
+                                <option value="5">მაისი</option>
+                                <option value="6">ივნისი</option>
+                                <option value="7">ივლისი</option>
+                                <option value="8">აგვისტო</option>
+                                <option value="9">სექტემბერი</option>
+                                <option value="10">ოქტომბერი</option>
+                                <option value="11">ნოემბერი</option>
+                                <option value="12">დეკემბერი</option>
+                            </select>
+                        </div>
+                        <div>
+                            <label for="start_year">საწყისი წელი</label>
+                            <input name="start_year" class="uk-input" id="start_year" type="number" min="1000" max="9999">
+                        </div>
                     </div>
-                    <div>
-                        <label for="enddate">End Date</label>
-                        <input type="date" name="enddate" id="enddate">
+                    <div class="uk-flex uk-flex-between" style="margin-top: 40px">
+                        <div>
+                            <label for="start_day">საბოლოო დღე</label>
+                            <input name="start_day" class="uk-input" id="start_day" type="number" min="1" max="31">
+                        </div>
+                        <div>
+                            <label for="start_month">საბოლოო თვე</label>
+                            <select name="start_month" class="uk-select" id="start_month">
+                                <option value="">აირჩიე თვე</option>
+                                <option value="1">იანვარი</option>
+                                <option value="2">თებერვალი</option>
+                                <option value="3">მარტი</option>
+                                <option value="4">აპრილი</option>
+                                <option value="5">მაისი</option>
+                                <option value="6">ივნისი</option>
+                                <option value="7">ივლისი</option>
+                                <option value="8">აგვისტო</option>
+                                <option value="9">სექტემბერი</option>
+                                <option value="10">ოქტომბერი</option>
+                                <option value="11">ნოემბერი</option>
+                                <option value="12">დეკემბერი</option>
+                            </select>
+                        </div>
+                        <div>
+                            <label for="start_year">საბოლოო წელი</label>
+                            <input name="start_year" class="uk-input" id="start_year" type="number" min="1000" max="9999">
+                        </div>
                     </div>
+
                 </div>
 
                 <div class="nominals" style="margin-top: 50px">
-                    <h1 class="uk-text-center">Nominals</h1>
+                    <h1 class="uk-text-center">ნომინალები</h1>
                     <?php foreach (range(1, 7) as $checkbox): ?>
                         <div class="uk-margin">
                             <?= checkbox('one' . $checkbox, 'One' . $checkbox, true) ?>
--- app/Views/document/index.php
+++ app/Views/document/index.php
@@ -1,7 +1,26 @@
 <?= $this->extend('base_view') ?>
 <?= $this->section('content') ?>
+<div class="uk-container">
+    <h1 class="uk-text-center"><?= $page_title ?></h1>
 
-<h1 class="uk-text-center"><?=$page_title?></h1>
+    <form action="">
 
+        <div class="uk-margin">
+            <label for="doc_number">დოკუმენტის ნომერი</label>
+            <input class="uk-input" type="number" id="doc_number">
+        </div>
+
+        <div class="uk-margin">
+            <label for="doc_name">დოკუმენტის სახელი</label>
+            <input class="uk-input" type="text" id="doc_name">
+        </div>
+
+        <div class="uk-text-right">
+            <button class="uk-button uk-button-danger">გაუქმება</button>
+            <button class="uk-button uk-button-primary ">შენახვა</button>
+        </div>
+
+    </form>
+</div>
 <?= $this->endSection() ?>
 
